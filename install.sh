c34d866ec3d97dc860e0161bcc8104dda497c4bf
XintSong <202117118@mail.sdu.edu.cn>
2024-04-14 17:29:36 +0800

    commit

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 conda activate base
-conda create -name rmsf-net python=3.8
+conda create --name rmsf-net python=3.8
 pip install -r requirements.txt
 conda install moleculekit  -c acellera -c conda-forge
